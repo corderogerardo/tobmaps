86903e78ccb8c798c9a5087b5c311e5b88820bfa
Gerardo Cordero <cordero.gerard@gmail.com>
2016-08-31 13:37:07 -0400

    Fix server/mains.js file.

--- server/main.js
+++ server/main.js
@@ -2,7 +2,11 @@
 // users have the username 'user1@test.com' .. 'user8@test.com'
 // and the password test123
  Meteor.startup(function () {
-    if (!Meteor.users.findOne()){
+ 	// bootstrap the admin user if they exist -- You'll be replacing the id later
+	if (Meteor.users.findOne("myQpmPqNZwsYNn6GW"))
+	  Roles.addUsersToRoles("myQpmPqNZwsYNn6GW", ['Admin']);
+
+	if (!Meteor.users.findOne()){
       for (var i=1;i<3;i++){
         var email = "user"+i+"@test.com";
         var avatar = "ava"+i+".png"
@@ -13,8 +17,3 @@
       }
     }
   });
-Meteor.startup(function () {
-	// bootstrap the admin user if they exist -- You'll be replacing the id later
-	if (Meteor.users.findOne("myQpmPqNZwsYNn6GW"))
-	  Roles.addUsersToRoles("myQpmPqNZwsYNn6GW", ['Admin']);
-})
