9ff4689592b4ed7fc24afe3e790d2decf9da932a
Fernando Salazar <fernando.jsr5@gmail.com>
2016-09-02 08:57:39 -0400

    correction in the js login and html login

--- client/views/user/login.js
+++ client/views/user/login.js
@@ -4,12 +4,11 @@
  * @return {[type]}
  */
 
-/*Template.loginForm.onRendered(function(){
+Template.loginForm.onRendered(function(){
     var validator = $('.js-form-login').validate({
-        submitHandler: function(event){
-        	event.preventDefault();
-            var email = $('#email').val();
-            var password = $('#password').val();
+        submitHandler: function(){
+            var email = $('[name=email]').val();
+            var password = $('[name=password]').val();
             console.log(" "+email+" "+password);
             Meteor.loginWithPassword(email, password, function(error){
                 if(error){
@@ -27,34 +26,8 @@
                       Router.go("user.show", {_id: Meteor.userId()});
                 }
             });
-            return false;
         }
     });
-});*/
-
-Template.loginForm.events({
-        "click #js-form-login": function(event){
-        	event.preventDefault();
-            var email = $('#email').val();
-            var password = $('#password').val();
-            console.log(" "+email+" "+password);
-            Meteor.loginWithPassword(email, password, function(error){
-                if(error){
-                  if(error.reason == "User not found"){
-                      /*validator.showErrors({
-                          email: "That email doesn't belong to a registered user."
-                      });*/
-                  }
-                  if(error.reason == "Incorrect password"){
-                     /* validator.showErrors({
-                          password: "You entered an incorrect password."
-                      });*/
-                  }
-                } else {
-                      Router.go("user.show", {_id: Meteor.userId()});
-            			return false;
-                }
-            });
-        }
 });
+
 
