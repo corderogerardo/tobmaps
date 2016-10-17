ac3e225ba25e95f9e2a0f74f0358adf9bb864bcf
Gerardo Cordero <cordero.gerard@gmail.com>
2016-10-17 10:01:28 -0400

    Documentation: conf.json and jsdoc command.

--- client/views/user/login.js
+++ client/views/user/login.js
@@ -6,57 +6,57 @@
  * @return {[Meteor.call(function(error))]}
  */
 
-Template.loginForm.onRendered(function(){
-    var validator = $('.js-form-login').validate({
-        submitHandler: function(){
-            var email = $('[name=email]').val();
-            var password = $('[name=password]').val();
-            console.log(" "+email+" "+password);
-            Meteor.loginWithPassword(email, password, function(error){
-                if(error){
-                  if(error.reason == "User not found"){
-                      validator.showErrors({
-                          email: "That email doesn't belong to a registered user."
-                      });
-                  }
-                  if(error.reason == "Incorrect password"){
-                      validator.showErrors({
-                          password: "You entered an incorrect password."
-                      });
-                  }
-                } else {
-                      Router.go("user.show", {_id: Meteor.userId()});
-                }
-            });
-        }
-    });
-});
+ Template.loginForm.onRendered(function(){
+	var validator = $('.js-form-login').validate({
+		submitHandler: function(){
+			var email = $('[name=email]').val();
+			var password = $('[name=password]').val();
+			console.log(" "+email+" "+password);
+			Meteor.loginWithPassword(email, password, function(error){
+				if(error){
+					if(error.reason == "User not found"){
+						validator.showErrors({
+							email: "That email doesn't belong to a registered user."
+						});
+					}
+					if(error.reason == "Incorrect password"){
+						validator.showErrors({
+							password: "You entered an incorrect password."
+						});
+					}
+				} else {
+					Router.go("user.show", {_id: Meteor.userId()});
+				}
+			});
+		}
+	});
+ });
 
 /**
  * setDefaults function define a default set of rules and error messages validate functions
  * @type {Object}
  */
-$.validator.setDefaults({
-    rules: {
-        email: {
-            required: true,
-            email: true
-        },
-        password: {
-            required: true,
-            minlength: 6
-        }
-    },
-    messages: {
-        email: {
-            required: "You must enter an email address.",
-            email: "You've entered an invalid email address."
-        },
-        password: {
-            required: "You must enter a password.",
-            minlength: "Your password must be at least {0} characters."
-        }
-    }
-});
+ $.validator.setDefaults({
+	rules: {
+		email: {
+			required: true,
+			email: true
+		},
+		password: {
+			required: true,
+			minlength: 6
+		}
+	},
+	messages: {
+		email: {
+			required: "You must enter an email address.",
+			email: "You've entered an invalid email address."
+		},
+		password: {
+			required: "You must enter a password.",
+			minlength: "Your password must be at least {0} characters."
+		}
+	}
+ });
 
 
--- client/views/user/register.js
+++ client/views/user/register.js
@@ -1,15 +1,13 @@
-'use strict';
-
 Template.register.rendered = function(){
-  // Initialize i-check plugin
-  $('.i-checks').iCheck({
-      checkboxClass: 'icheckbox_square-green',
-      radioClass: 'iradio_square-green'
-  });
+	// Initialize i-check plugin
+	$('.i-checks').iCheck({
+		checkboxClass: 'icheckbox_square-green',
+		radioClass: 'iradio_square-green'
+	});
 
-  // Move modal to body
-  // Fix Bootstrap backdrop issu with animation.css
-  $('.modal').appendTo("body");
+	// Move modal to body
+	// Fix Bootstrap backdrop issu with animation.css
+	$('.modal').appendTo("body");
 };
 
 /**
@@ -19,57 +17,57 @@ Template.register.rendered = function(){
  * @param  {[name='password']} )
  * @return {[Meteor.call(function(error))]}
  */
-Template.register.onRendered(function(){
-  var validator = $('.js-register-form').validate({
-    submitHandler: function(){
-      var email = $('[name=email]').val();
-      var password = $('[name=password]').val();
-      var account = {
-        email:email,
-        password:password,
-      };
-      Meteor.call('addAccount',account, function(error){
-        if(error){
-            if(error.reason == "Email already exists."){
-                validator.showErrors({
-                    email: "That email already belongs to a registered user."   
-                });
-            }
-        } else {
-            toastr.success('You have create a new account.','User added!');
-            $('.js-register-form').trigger('reset');
-        }
-      });
-    }    
-  });
-});
+ Template.register.onRendered(function(){
+	var validator = $('.js-register-form').validate({
+		submitHandler: function(){
+			var email = $('[name=email]').val();
+			var password = $('[name=password]').val();
+			var account = {
+				email:email,
+				password:password,
+			};
+			Meteor.call('addAccount',account, function(error){
+				if(error){
+					if(error.reason == "Email already exists."){
+						validator.showErrors({
+							email: "That email already belongs to a registered user."
+						});
+					}
+				} else {
+					toastr.success('You have create a new account.','User added!');
+					$('.js-register-form').trigger('reset');
+				}
+			});
+		}
+	});
+ });
 
 /**
  * setDefaults function define a default set of rules and error messages validate functions
  * @type {Object}
  */
-$.validator.setDefaults({
-    rules: {
-        email: {
-            required: true,
-            email: true
-        },
-        password: {
-            required: true,
-            minlength: 6
-        }
-    },
-    messages: {
-        email: {
-            required: "You must enter an email address.",
-            email: "You've entered an invalid email address."
-        },
-        password: {
-            required: "You must enter a password.",
-            minlength: "Your password must be at least {0} characters."
-        }
-    }
-});
+ $.validator.setDefaults({
+	rules: {
+		email: {
+			required: true,
+			email: true
+		},
+		password: {
+			required: true,
+			minlength: 6
+		}
+	},
+	messages: {
+		email: {
+			required: "You must enter an email address.",
+			email: "You've entered an invalid email address."
+		},
+		password: {
+			required: "You must enter a password.",
+			minlength: "Your password must be at least {0} characters."
+		}
+	}
+ });
 
 
 
